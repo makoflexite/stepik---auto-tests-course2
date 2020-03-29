8e8fa5f66560aafc268a95bb78bf9aaa38646e28
Mariya <mako@flexite.com>
2020-03-29 18:45:02 +0300

    Explicit Waits task

---
+++ test8.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    price = WebDriverWait(browser, 15).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+
+    button = browser.find_element_by_class_name("btn")
+    button.click()
+
+    # new_window = browser.window_handles[1]
+    # browser.switch_to_window(new_window)
+    #
+    field1 = browser.find_element_by_id("input_value")
+    x = field1.text
+    y = calc(x)
+
+    input2 = browser.find_element_by_id("answer")
+    input2.send_keys(y)
+
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
