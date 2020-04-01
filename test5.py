d98ffcfd91ca3a0d0798ba4c9bcf12482883123c
Mariya <mako@flexite.com>
2020-04-01 13:19:14 +0300

    test directory

---
+++ test5.py
@@ -0,0 +1,32 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+
+try:
+    link = "http://suninjuly.github.io/file_input.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_xpath("//input[@placeholder='Enter first name']")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_xpath("//input[@placeholder='Enter last name']")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_xpath("//input[@placeholder='Enter email']")
+    input3.send_keys("1@1.1")
+    file = browser.find_element_by_tag_name("[type = 'file']")
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(current_dir, 'file.txt')
+    file.send_keys(file_path)
+
+    button = browser.find_element_by_class_name("btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
