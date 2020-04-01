c4e33d0f06a26736ba8b30633b30f931202d5db5
Mariya <mako@flexite.com>
2020-04-01 13:19:32 +0300

    test directory

---
+++ test3.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_id("input_value")
+    x = int(input1.text)
+    y = calc(x)
+
+    input2 = browser.find_element_by_id("answer")
+    input2.send_keys(y)
+
+    option1 = browser.find_element_by_id("robotCheckbox")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", option1)
+    option1.click()
+
+    option2 = browser.find_element_by_id("robotsRule")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", option2)
+    option2.click()
+
+    button = browser.find_element_by_class_name("btn")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
